11ddd741244379a44b6b310fe1a105c421d60c4d
andrezacm <andreza.cmedeiros@gmail.com>
2015-01-27 22:09:50 -0800

    Basic project on Xcode

---
+++ assignment2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  assignment2
+//
+//  Created by Andreza da Costa Medeiros on 1/27/15.
+//  Copyright (c) 2015 Andreza da Costa Medeiros. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
